78348b0f0bb185a0b447e5535320dfedbf3ef1b2
Vishal kumar <vksingh829842@gmail.com>
2026-04-07 14:44:07 +0530

    Update server.js

--- server.js
+++ server.js
@@ -8,13 +8,34 @@ const io = new Server(server);
 
 app.use(express.static("public"));
 
+let users = {};
+
 io.on("connection", (socket) => {
-  socket.on("chat message", (msg) => {
-    io.emit("chat message", msg);
+
+  socket.on("join", (username) => {
+    users[socket.id] = username;
+    io.emit("users", Object.values(users));
   });
+
+  socket.on("chat message", (msg) => {
+    io.emit("chat message", {
+      user: users[socket.id],
+      text: msg
+    });
+  });
+
+  socket.on("typing", () => {
+    socket.broadcast.emit("typing", users[socket.id] + " is typing...");
+  });
+
+  socket.on("disconnect", () => {
+    delete users[socket.id];
+    io.emit("users", Object.values(users));
+  });
+
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 
 server.listen(PORT, "0.0.0.0", () => {
   console.log("Server running...");
