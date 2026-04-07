8e8a415032a5dccf66fa8dd7de5b0c27a6a6df69
Vishal kumar <vksingh829842@gmail.com>
2026-04-07 15:47:39 +0530

    Update server.js

--- server.js
+++ server.js
@@ -14,28 +14,36 @@ io.on("connection", (socket) => {
 
   socket.on("join", (username) => {
     users[socket.id] = username;
+
+    io.emit("status", username + " is online");
     io.emit("users", Object.values(users));
   });
 
   socket.on("chat message", (msg) => {
     io.emit("chat message", {
       user: users[socket.id],
-      text: msg
+      text: msg,
+      seen: false
     });
   });
 
+  socket.on("seen", () => {
+    io.emit("seen update");
+  });
+
   socket.on("typing", () => {
-    socket.broadcast.emit("typing", users[socket.id] + " is typing...");
+    socket.broadcast.emit("typing", users[socket.id] + " typing...");
   });
 
   socket.on("disconnect", () => {
+    io.emit("status", users[socket.id] + " is offline");
     delete users[socket.id];
     io.emit("users", Object.values(users));
   });
 
 });
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3000;
 
 server.listen(PORT, "0.0.0.0", () => {
   console.log("Server running...");
